0b5b8a8c78e75c93b44244833b34210a3e786115
langshen <langshen2008@qq.com>
2017-12-21 14:44:23 +0800

    fix

---
+++ Extend/PhpExcel.class.php
@@ -0,0 +1,13 @@
+<?php
+namespace Spartan\Extend;
+
+/**
+ * Class PhpExcel
+ * 项目地址：https://github.com/PHPOffice/PHPExcel
+ */
+class PhpExcel{
+
+
+}
+
+
---
+++ Extend/WeChat.class.php
@@ -0,0 +1,11 @@
+<?php
+namespace Spartan\Extend;
+
+/**
+ * Class PhpExcel
+ * 项目地址：https://github.com/dodgepudding/wechat-php-sdk
+ */
+class WeChat{
+
+
+}
